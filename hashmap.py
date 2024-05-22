c478afbecaaa8b8d2d4392037a08324d1f9a208e
Yuchen Liu <lyc990312@hotmail.com>
2024-05-22 21:52:17 +1000

    Add the comment

--- hashmap.py
+++ hashmap.py
@@ -4,16 +4,21 @@ class HashMap():
         self.bucket = [[] for _ in range(size)]
 
     def get_hash(self, key):
+        # use hash function and size to get the index 
         return hash(key) % self.size
 
     def insert(self, key, value):
         hashed_key = self.get_hash(key)
+        # locate the bucket with hashed_key
         bucket = self.bucket[hashed_key]
+        # search for the key in the bucket
         for index, data in enumerate(bucket):
             data_key, data_value = data
+            # if key found
             if (data_key == key):
                 bucket[index] = (key, value)
                 return
+        # if key not found, append new tuple
         bucket.append((key, value))
     
     def delete(self, key):
@@ -36,6 +41,6 @@ class HashMap():
     def __str__(self):
         items = []
         for i, bucket in enumerate(self.bucket):
-            if bucket:
-                items.append(f"Bucket {i}: {bucket}")
+
+            items.append(f"Bucket {i}: {bucket}")
         return "\n".join(items)
