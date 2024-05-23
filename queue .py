d7aad40cd552a6d325c6a3bedfc99cbbd50812dc
Yuchen Liu <lyc990312@hotmail.com>
2024-05-23 23:16:05 +1000

    Finish the queue with deque()

---
+++ queue .py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Queue():
+    def __init__(self) -> None:
+        self.queue = deque()
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.popleft()
+        else:
+            return "Queue is empty"
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.queue[0]
+        else:
+            return "Queue is empty"
+        
+    def size(self):
+        return len(self.queue)
+
+
