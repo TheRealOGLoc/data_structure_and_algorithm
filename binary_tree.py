01a410b895a0b402ed0af8d622c9ee0a7d94dcb9
Yuchen Liu <lyc990312@hotmail.com>
2024-06-25 21:36:19 +1000

    Finish a basic binary tree structure

--- binary_tree.py
+++ binary_tree.py
@@ -15,3 +15,25 @@
 #     3
 #   /   \
 #  1     2
+
+class BinarySearchTreeNode:  
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if self.data == data: #binary search tree can not have same value
+            return 
+        
+        if data < self.left:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    
