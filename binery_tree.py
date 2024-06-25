4853b2a62311e1227a5a98c08495637cd87f7968
Yuchen Liu <lyc990312@hotmail.com>
2024-06-25 21:25:29 +1000

    Create the binary tree, and add the description of each traversal

---
+++ binery_tree.py
@@ -0,0 +1,17 @@
+#in order traversal:
+#In this traversal method, the left subtree is visited first, then the root and later the right sub-tree.
+#     2
+#   /   \
+#  1     3
+  
+#pre order traversal:
+#in this traversal method, the root node is visited first, then the left subtree and finally right subtree.
+#     1
+#   /   \
+#  2     3
+
+#post order traversal:
+#in this traversal method, we first traverse the left subtree, then the right subtree and finally the root node.
+#     3
+#   /   \
+#  1     2
